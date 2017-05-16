63339ec0bc4772084f7a0611d1746dbcb35e5bf3
Gavin Day <mngad@leeds.ac.uk>
2017-05-16 15:26:18 +0100

    formatting and comments

--- pyRss.py
+++ pyRss.py
@@ -1,39 +1,8 @@
 import feedparser
 from future import Future
 
-directory = '/home/fraun/start/rss'
-
-
-def getLinks():
-
-    urllist = []
-    urlFile = open(directory + '/urlFile.txt', 'r')
-    for line in urlFile:
-        urllist.append(line)
-    future_calls = [Future(feedparser.parse, rss_url) for rss_url in urllist]
-
-    feeds = [future_obj() for future_obj in future_calls]
-    linkFormatted = ''
-
-    for feed in feeds:
-        print(feed["channel"]["title"])
-        linkFormatted = linkFormatted + '<p> <a href="' + \
-            feed['url'] + '">' + feed['channel']['title'] + '</a> </p>' + '\n'
-        count = 0
-        for entry in feed['items']:
-            if count == 5:
-                print('\n')
-                break
-            else:
-                count = count + 1
-                title = entry['title']
-                link = entry['link']
-                print('{}, [{}]'.format(entry["title"], entry['link']))
-                linkFormatted = linkFormatted + '<a href="' + \
-                    link + '">' + ' - ' + title + '</a><br>' + '\n'
-    return linkFormatted
-
-
+directory = '/home/fraun/start/rss'  # directory where the index file will be
+# created and where the urlList and css files are found.
 template = """<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -54,6 +23,37 @@ template = """<!DOCTYPE html>
 </body>
 </html>
 """
+
+
+def getLinks():  # get the urls, feeds, links and link titles
+
+    urllist = []
+    urlFile = open(directory + '/urlFile.txt', 'r')
+    for line in urlFile:
+        urllist.append(line)
+    future_calls = [Future(feedparser.parse, rss_url) for rss_url in urllist]
+    # multithread the download of the urls
+    feeds = [future_obj() for future_obj in future_calls]
+    linkFormatted = ''
+
+    for feed in feeds:  # cycle through feeds (each url)
+        print(feed["channel"]["title"])
+        # create title for each feed
+        linkFormatted = linkFormatted + '<p> <a href="' + \
+            feed['url'] + '">' + feed['channel']['title'] + '</a> </p>' + '\n'
+        count = 0
+        for entry in feed['items']:
+            if count == 5:
+                print('\n')
+                break
+            else:  # create links for top five from each feed
+                count = count + 1
+                title = entry['title']
+                link = entry['link']
+                print('{}, [{}]'.format(entry["title"], entry['link']))
+                linkFormatted = linkFormatted + '<a href="' + \
+                    link + '">' + ' - ' + title + '</a><br>' + '\n'
+    return linkFormatted
 
 
 if __name__ == '__main__':
