c56dc74753c750b8685c83166727bdbdcf513282
Gavin Day <mngad@leeds.ac.uk>
2017-05-22 16:20:13 +0100

    pep8 formatting

--- pyRss.py
+++ pyRss.py
@@ -1,27 +1,25 @@
+
 import feedparser
+
 from future import Future
-import os
-from shutil import copyfile
 
-
-cwd = os.getcwd()
-rssURL = 'http://fraun.space/rss'
-directory = '/srv/http/rss'  # directory where the index file will be
+directory = '/home/fraun/start/rss'  # directory where the index file will be
 # created and where the urlList and css files are found.
-
 template = """<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
     <link rel="icon" type="image/png" href="/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="minimal_{}.css" />
-    <title>{}</title>
+    <link rel="stylesheet" type="text/css" href="minimal.css" />
+    <title>RSS</title>
 </head>
 <body>
     <div id="content">
 
-        {links}
+        <ul id="index">
+            {links}
+        </ul>
 
     </div>
 </body>
@@ -29,62 +27,22 @@ template = """<!DOCTYPE html>
 """
 
 
-def remBadChar(string):
-    string = ''.join(c for c in string if c not in ' /\\(){}<>\'!')
-    return string
-
-
-def makePage(pageTitle, linkFormatted, htmlTitle, cssType):
-
-    context = {  # context for the tmplate - bits to change - filname etc.
-        "links": linkFormatted
-    }
-    indexFile = open(directory + '/' + pageTitle, 'w')
-    indexFile.write(template.format(cssType, htmlTitle, **context))
-    indexFile.close()
-
-
-def makeIndiePages(feed):
-    linkFormatted = ''
-    linkFormatted = linkFormatted + '<p> <a href="' + \
-        feed['url'] + '">' + feed['channel']['title'] + '</a> </p>' + '\n'
-    count = 0
-    for entry in feed['items']:
-        if count == 100:
-           #  print('\n')
-            break
-        else:  # create links for top five from each feed
-            count = count + 1
-            title = entry['title']
-            link = entry['link']
-            # print('{}, [{}]'.format(entry["title"], entry['link']))
-            linkFormatted = linkFormatted + '<a href="' + \
-                link + '">' + ' - ' + title + '</a><br>' + '\n'
-
-    linkFormatted = linkFormatted + '<p> <a href="' + \
-        rssURL + '">' + '<<< BACK' + '</a> </p>' + '\n'
-    makePage(remBadChar(feed['channel']['title']) + '.html', linkFormatted,
-            feed['channel']['title'], 'noCol')
-
-
-def getLinks():  # get the urls, feeds, links and link titles
+def get_links():  # get the urls, feeds, links and link titles
 
     urllist = []
-    urlFile = open(directory + '/urlFile.txt', 'r')
-    for line in urlFile:
+    url_file = open(directory + '/urlFile.txt', 'r')
+    for line in url_file:
         urllist.append(line)
     future_calls = [Future(feedparser.parse, rss_url) for rss_url in urllist]
     # multithread the download of the urls
     feeds = [future_obj() for future_obj in future_calls]
-    linkFormatted = ''
+    link_formatted = ''
 
     for feed in feeds:  # cycle through feeds (each url)
         print(feed["channel"]["title"])
         # create title for each feed
-        makeIndiePages(feed)
-        linkFormatted = linkFormatted + '<p> <a href="' + rssURL + '/' + \
-            remBadChar(feed['channel']['title']) + '.html' + '">' + \
-            feed['channel']['title'] + '</a> </p>' + '\n'
+        link_formatted = link_formatted + '<p> <a href="' + \
+            feed['url'] + '">' + feed['channel']['title'] + '</a> </p>' + '\n'
         count = 0
         for entry in feed['items']:
             if count == 5:
@@ -94,17 +52,18 @@ def getLinks():  # get the urls, feeds, links and link titles
                 count = count + 1
                 title = entry['title']
                 link = entry['link']
-                # print('{}, [{}]'.format(entry["title"], entry['link']))
-                linkFormatted = linkFormatted + '<a href="' + \
+                print('{}, [{}]'.format(entry["title"], entry['link']))
+                link_formatted = link_formatted + '<a href="' + \
                     link + '">' + ' - ' + title + '</a><br>' + '\n'
-    return linkFormatted
+    return link_formatted
 
 
 if __name__ == '__main__':
-
-    linkFormatted = getLinks()
-    makePage('index.html', linkFormatted, 'RSS', 'Col')
-    if not os.path.isfile(directory + '/minimal_Col.css'):
-        copyfile(cwd + '/minimal_Col.css', directory + '/minimal_Col.css')
-    if not os.path.isfile(directory + '/minimal_noCol.css'):
-        copyfile(cwd + '/minimal_noCol.css', directory + '/minimal_noCol.css')
+    link_formatted = ''
+    link_formatted = get_links()
+    context = {  # context for the tmplate - bits to change - filname etc.
+        "links": link_formatted
+    }
+    index_file = open(directory + '/index.html', 'w')
+    index_file.write(template.format(**context))
+    index_file.close()
